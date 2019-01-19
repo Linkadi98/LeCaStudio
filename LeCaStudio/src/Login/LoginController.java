66034283441fcb43e54520e7ce8173feb3dc3953
Pham Ngoc Minh <minhngock18@gmail.com>
2019-01-19 16:11:44 +0700

    Login Updated UI

--- LeCaStudio/src/Login/LoginController.java
+++ LeCaStudio/src/Login/LoginController.java
@@ -1,12 +1,24 @@
 package Login;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.animation.FadeTransition;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.util.Duration;
 
 public class LoginController {
 
+    @FXML
+    public AnchorPane achorPane;
     @FXML
     private ResourceBundle resources;
 
@@ -14,7 +26,29 @@ public class LoginController {
     private URL location;
 
     @FXML
+    private JFXTextField adminName;
+
+    @FXML
+    private JFXPasswordField password;
+
+    @FXML
+    void adminNameKeyPressed(KeyEvent event) {
+        if(event.getCode() == KeyCode.ENTER) {
+            if (adminName.getText() == "") {
+                return;
+            } else {
+                password.requestFocus();
+            }
+        }
+
+    }
+
+    @FXML
     void cancel(ActionEvent event) {
+        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(3), achorPane);
+        fadeTransition.setFromValue(2.0);
+        fadeTransition.setToValue(0.3);
+        fadeTransition.play();
         System.exit(0);
     }
 
@@ -25,6 +59,22 @@ public class LoginController {
 
     @FXML
     void initialize() {
+        assert adminName != null : "fx:id=\"adminName\" was not injected: check your FXML file 'Login.fxml'.";
+        assert password != null : "fx:id=\"password\" was not injected: check your FXML file 'Login.fxml'.";
 
+    }
+    @FXML
+    public void passwordKeyPressed(KeyEvent keyEvent) {
+        if(keyEvent.getCode() == KeyCode.ENTER) {
+            if(adminName.getText().equals("LeCanh") && password.getText().equals("1234")) {
+                System.out.println("Thanh cong");
+            }
+            else {
+                adminName.setText("");
+                password.setText("");
+                adminName.setFocusColor(Color.RED);
+                password.setFocusColor(Color.RED);
+            }
+        }
     }
 }
